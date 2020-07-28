146601882d38daa64ee2e2960bc062fba4905132
Mihai Cucui <mihai.cucui98@yahoo.ro>
2020-07-28 23:46:26 +0300

    Timer class solved: start function works

--- chessJS.js
+++ chessJS.js
@@ -1,11 +1,33 @@
 let countDownP = document.createElement('p');
-countDownP.textContent = 'new';
+//countDownP.textContent = 'new';
 countDownP.id = 'countDownP';
 
 let mainDiv = document.createElement('div');
 mainDiv.classList.add('main-div')
 
-let seconds = 5;
+
+
+function addChessTableToDiv(container) {
+
+    let gridDiv = document.createElement('div');
+    gridDiv.id = "grid-div";
+    gridDiv.classList.add('grid-div');
+    container.appendChild(gridDiv);
+
+    //countDownP.parentNode.removeChild(countDownP);
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            let createdDiv = document.createElement('div');
+            gridDiv.appendChild(createdDiv);
+            if ((i + j) % 2 == 0) {
+                createdDiv.classList.add('white-div')
+            }
+            else {
+                createdDiv.classList.add('black-div')
+            }
+        }
+    }
+}
 
 let body = document.getElementsByTagName('body')[0].appendChild(mainDiv);
 mainDiv.appendChild(countDownP);
@@ -21,9 +43,7 @@ mainDiv.appendChild(countDownP);
 //     }
 // }
 
-const countdown = new Timer(5, countDownP, () => {
-    addChessTableToDiv(mainDiv);
-})
+
 
 
 class Timer {
@@ -36,7 +56,7 @@ class Timer {
      */
     constructor(seconds = 5, paragraph = null, endCallback = null) {
         this.seconds = seconds;
-        this.paragraph = this.paragraph;
+        this.paragraph = paragraph;
         this.endCallback = endCallback;
 
     }
@@ -48,6 +68,7 @@ class Timer {
 
     decrement() {
         this.seconds--;
+        console.log(this.seconds);
     }
     start() {
         const countdown = setInterval(() => {
@@ -62,37 +83,18 @@ class Timer {
                     this.endCallback();
                 }
             } else {
+                this.paragraph.textContent=this.seconds;
                 this.decrement();
-
-
             }
         }, 1000);
     }
 
 }
 
+const countdown = new Timer(5, countDownP, () => {
+    addChessTableToDiv(mainDiv);
+})
 
-function addChessTableToDiv(container) {
-
-    let gridDiv = document.createElement('div');
-    gridDiv.id = "grid-div";
-    gridDiv.classList.add('grid-div');
-    container.appendChild(gridDiv);
-
-    countDownP.parentNode.removeChild(countDownP);
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            let createdDiv = document.createElement('div');
-            gridDiv.appendChild(createdDiv);
-            if ((i + j) % 2 == 0) {
-                createdDiv.classList.add('white-div')
-            }
-            else {
-                createdDiv.classList.add('black-div')
-            }
-        }
-    }
-}
 
 
 
