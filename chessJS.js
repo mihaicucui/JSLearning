edc0ff9e21cfe1df11de0b98eeabf5e493711f00
Mihai Cucui <mihai.cucui@bitstone.eu>
2020-07-30 11:40:47 +0300

    Selected items and remove piece added

--- chessJS.js
+++ chessJS.js
@@ -7,42 +7,114 @@ mainDiv.classList.add('main-div')
 
 
 
-function addChessTableToDiv(container) {
+let body = document.getElementsByTagName('body')[0].appendChild(mainDiv);
+mainDiv.appendChild(countDownP);
 
-    let gridDiv = document.createElement('div');
-    gridDiv.id = "grid-div";
-    gridDiv.classList.add('grid-div');
-    container.appendChild(gridDiv);
 
-    //countDownP.parentNode.removeChild(countDownP);
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            let createdDiv = document.createElement('div');
-            gridDiv.appendChild(createdDiv);
-            if ((i + j) % 2 == 0) {
-                createdDiv.classList.add('white-div')
+
+class ChessTable {
+    static initialState = [
+        ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+        ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+        ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']];
+
+    constructor(chessMatrix = null, gridDiv = null) {
+        this.chessMatrix = [];
+        for (let i = 0; i < 8; i++) {
+            this.chessMatrix[i] = [];
+            for (let j = 0; j < 8; j++) {
+                this.chessMatrix[i][j] = null;
             }
-            else {
-                createdDiv.classList.add('black-div')
+        }
+    }
+
+    
+
+    createTable() {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                this.chessMatrix[i][j] = new Square(i, j);
+                this.chessMatrix[i][j].elem.addEventListener('click', ()=> {
+                    console.log(this.chessMatrix[i][j].removePiece());
+                })
+            }
+        }
+
+
+    }
+
+    
+
+    addPieces() {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                let actualPiece = null;
+                switch (ChessTable.initialState[i][j]) {
+                    case 'bR':
+                        actualPiece = new BlackTower();
+                        break;
+                    case 'bN':
+                        actualPiece=new BlackHorse();
+                        break;
+                    case 'bB':
+                        actualPiece=new BlackBishop();
+                        break;
+                    case 'bQ':
+                        actualPiece=new BlackQueen();
+                        break;
+                    case 'bK':
+                        actualPiece=new BlackKing();
+                        break;
+                    case 'bP':
+                        actualPiece=new BlackPawn();
+                        break;
+                    case 'wR':
+                        actualPiece=new WhiteTower();
+                        break;
+                    case 'wN':
+                        actualPiece=new WhiteHorse();
+                        break;
+                    case 'wB':
+                        actualPiece=new WhiteBishop();
+                        break;
+                    case 'wQ':
+                        actualPiece=new WhiteQueen();
+                        break;
+                    case 'wK':
+                        actualPiece=new WhiteKing();
+                        break;
+                    case 'wP':
+                        actualPiece=new WhitePawn();
+                        break;
+                }
+                this.chessMatrix[i][j].setPiece(actualPiece);
+            }
+        }
+    }
+
+    drawTable(container) {
+        if (container) {
+            let gridDiv = document.createElement('div');
+            gridDiv.id = "grid-div";
+            gridDiv.classList.add('grid-div');
+            container.appendChild(gridDiv);
+
+            this.createTable();
+            this.addPieces();
+
+            for (let i = 0; i < 8; i++) {
+                for (let j = 0; j < 8; j++) {
+                    gridDiv.appendChild(this.chessMatrix[i][j].elem);
+                }
             }
         }
     }
 }
-
-let body = document.getElementsByTagName('body')[0].appendChild(mainDiv);
-mainDiv.appendChild(countDownP);
-
-// function Decrement() {
-//     countDownP.textContent = seconds;
-//     seconds--;
-//     if (seconds >= 0) {
-//         setTimeout('Decrement()', 1000);
-//     }
-//     else {
-//         addChessTableToDiv(mainDiv);
-//     }
-// }
-
 
 
 
@@ -54,14 +126,14 @@ class Timer {
      * @param {Element} paragraph 
      * @param {Function} endCallback 
      */
-    constructor(seconds = 5, paragraph = null, endCallback = null) {
+    constructor(seconds = 3, paragraph = null, endCallback = null) {
         this.seconds = seconds;
         this.paragraph = paragraph;
         this.endCallback = endCallback;
 
     }
 
-    
+
     getSeconds() {
         return this.seconds;
     }
@@ -83,7 +155,7 @@ class Timer {
                     this.endCallback();
                 }
             } else {
-                this.paragraph.textContent=this.seconds;
+                this.paragraph.textContent = this.seconds;
                 this.decrement();
             }
         }, 1000);
@@ -91,9 +163,137 @@ class Timer {
 
 }
 
-const countdown = new Timer(5, countDownP, () => {
-    addChessTableToDiv(mainDiv);
+class Square {
+    constructor(xCoord = null, yCoord = null, elem = null, piece = null) {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.piece = piece;
+
+        this.elem = document.createElement('div');
+        if ((xCoord + yCoord) % 2 == 0) {
+            this.elem.classList.add('white-div');
+        }
+        else {
+            this.elem.classList.add('black-div')
+        }
+
+        this.elem.tabIndex="1";
+        
+        
+
+    }
+
+    setPiece(piece) {
+        if (piece != null && piece != undefined) {
+            this.piece = piece;
+            this.elem.appendChild(piece.elem);
+            console.log(piece.elem + " " + this.xCoord + " " + this.yCoord);
+        }
+    }
+
+    removePiece(){
+        if(this.elem.childNodes[0]){
+            let toRemove = this.elem.removeChild(this.elem.childNodes[0]);
+        return toRemove;
+        }
+        else return null;
+    }
+
+
+}
+
+class Piece {
+    constructor(elem = null) {
+        this.elem = document.createElement('img');
+        this.elem.style.marginTop = "10px";
+        this.elem.src = this.constructor.getImage();
+    }
+
+}
+
+
+class WhiteBishop extends Piece {
+    static getImage() {
+        return 'bsWhite.png';
+    }
+
+    canMove(){
+
+    }
+}
+
+class BlackBishop extends Piece {
+    static getImage() {
+        return 'bsBlack.png';
+    }
+}
+
+class WhiteTower extends Piece {
+    static getImage() {
+        return 'towerWhite.png';
+    }
+}
+
+class BlackTower extends Piece {
+    static getImage() {
+        return 'towerBlack.png';
+    }
+}
+
+class WhiteHorse extends Piece {
+    static getImage() {
+        return 'horseWhite.png';
+    }
+}
+
+class BlackHorse extends Piece {
+    static getImage() {
+        return 'horseBlack.png';
+    }
+}
+
+class WhiteKing extends Piece {
+    static getImage() {
+        return 'kWhite.png';
+    }
+}
+
+class BlackKing extends Piece {
+    static getImage() {
+        return 'kBlack.png';
+    }
+}
+
+class WhiteQueen extends Piece {
+    static getImage() {
+        return 'queenWhite.png';
+    }
+}
+
+class BlackQueen extends Piece {
+    static getImage() {
+        return 'queenBlack.png';
+    }
+}
+
+class WhitePawn extends Piece {
+    static getImage() {
+        return 'pawnWhite.png';
+    }
+}
+
+class BlackPawn extends Piece {
+    static getImage() {
+        return 'pawnBlack.png';
+    }
+}
+
+const chessTable = new ChessTable();
+
+const countdown = new Timer(1, countDownP, () => {
+    chessTable.drawTable(mainDiv)
 })
+
 
 
 
